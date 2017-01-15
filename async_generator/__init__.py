49809e36ba2c6feca3e8b554a4d249bb603ff751
Nathaniel J. Smith <njs@pobox.com>
2017-01-15 02:45:40 -0800

    bump version to 1.5+dev

--- async_generator/__init__.py
+++ async_generator/__init__.py
@@ -3,7 +3,7 @@ from .impl import (
 )
 from .util import aclosing
 
-__version__ = "1.5"
+__version__ = "1.5+dev"
 
 __all__ = ["async_generator", "yield_", "yield_from_", "aclosing",
            "isasyncgen", "isasyncgenfunction"]
